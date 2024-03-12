19513e13b4dd27ad09ba6a344635cac1bcb12843
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-03-12 17:40:43 +0100

    add trains cards view

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,6 +10,19 @@
       @vite('resources\js\app.js')
 </head>
 <body>
-    <h1>Test</h1>
+      <header class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <div id="nav-title" class="navbar-brand fs-2">{{ env('APP_NAME')}}</div>
+        </div>
+    </header>
+    
+    @yield('main-content')
+
+    <footer class="bg-dark text-light py-4">
+    <div class="container text-center">
+        <p>© 2024 {{ env('APP_NAME')}}. Tutti i diritti riservati.</p>
+    </div>
+</footer>
+
 </body>
 </html>
--- resources/views/trains.blade.php
+++ resources/views/trains.blade.php
@@ -1,3 +1,31 @@
 @extends('layouts.main')
 
 @section('title', 'Trains List')
+
+@section('main-content')
+    <div class="container">
+        <h1 class="mt-4 mb-3">Trains List</h1>
+        <div class="row">
+            @foreach($trains as $train)
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title"><strong>Train number:</strong> {{ $train->train_code }}</h5>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text"><strong>Agency:</strong> {{ $train->agency }}</p>
+                            <p class="card-text"><strong>Departure Station:</strong> {{ $train->departure_station }}</p>
+                            <p class="card-text"><strong>Arrival Station:</strong> {{ $train->arrival_station }}</p>
+                            <p class="card-text"><strong>Departure Time:</strong> {{ $train->departure_time }}</p>
+                            <p class="card-text"><strong>Arrival Time:</strong> {{ $train->arrival_time }}</p>
+                            <p class="card-text"><strong>Number of Carriages:</strong> {{ $train->num_carriages }}</p>
+                            <p class="card-text"><strong>In Time:</strong> {{ $train->in_time ? 'Yes' : 'No' }}</p>
+                            <p class="card-text"><strong>Deleted:</strong> {{ $train->deleted ? 'Yes' : 'No' }}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
+</strong>
